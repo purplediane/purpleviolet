91a2a127c607726f38cb28b53475f704a9021864
Diane Chen <dianedc@purpleweb.com>
2015-12-03 21:51:34 -0800

    More display changes, border on list, etc.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
